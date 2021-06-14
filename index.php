82d13ee8861ddc7661b7187f5b821dedad9af42b
Vaibhav Raj Sisodiya <vaibhavrajsisodiya13@gmail.com>
2021-06-14 22:11:01 +0530

    "Cloud Experiment"

--- index.php
+++ index.php
@@ -4,8 +4,10 @@
         <title>Web Hosting</title>
     </head>
     <body>
-        <div style="text-align: center;">
-            <h1 style="font-size: 250px;">Welcome</h1>
+        <div style="text-align: center;margin-top: 180px;">
+            <h1 style="font-size: 100px; color: red;">Cloud Experiment Heroku</h1>
+            <h1 style="font-size: 100px;color: blue;">Vaibhav Raj Sisodiya</h1>
+            
         </div>
     </body>
 </html>
