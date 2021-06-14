381250d29751040d94803386b42e395c60108ed9
Vaibhav Raj Sisodiya <vaibhavrajsisodiya13@gmail.com>
2021-06-14 19:29:25 +0530

    Cloud Experiment

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Web Hosting</title>
+    </head>
+    <body>
+        <div style="text-align: center;">
+            <h1 style="font-size: 250px;">Welcome</h1>
+        </div>
+    </body>
+</html>
